9d05c85002dd3abfe4c44349ecdb90d67d16fb2c
Filip Schouwenaars <filip.schouwenaars@gmail.com>
2015-04-09 11:29:24 -0400

    fix in startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onAttach <- function(...) {
   packageStartupMessage("author_course(): Create a course template to get you started.\n",
-                        "datacamp_login(): log in to DataCamp from within R.",
+                        "datacamp_login(): log in to DataCamp from within R.\n",
                         "upload_course(): upload or update your course file on DataCamp.\n",
                         "upload_chapter(): upload or update your chapter files on DataCamp.\n",
                         "For more information on course creation, visit https://www.teach.datacamp.com.\n")
