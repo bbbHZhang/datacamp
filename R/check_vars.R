c363dcdbea1e98cb7f6a38deee462788e818cd50
Filip Schouwenaars <filip.schouwenaars@gmail.com>
2015-10-14 11:56:58 +0200

    clean up

--- R/check_vars.R
+++ R/check_vars.R
@@ -8,9 +8,9 @@ max_num_ass_char <- 800
 linters_to_ignore <- c("trailing_blank_lines_linter", "trailing_whitespace_linter", "line_length_linter", "camel_case_linter")
 
 # how long each exercise takes to complete, in minutes
-timing_lut <- c(VideoExercise = 5, 
-                NormalExercise = 3, 
-                MultipleChoiceExercise = 2, 
-                ChallengeExercise = 4, 
+timing_lut <- c(VideoExercise = 5,
+                NormalExercise = 3,
+                MultipleChoiceExercise = 2,
+                ChallengeExercise = 4,
                 MarkdownExercise = 3)
 max_time <- 60
