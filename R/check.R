0b9109e8d53cf36fe7e361c34bb9347bccde6eb2
Filip Schouwenaars <filip.schouwenaars@gmail.com>
2016-01-26 11:41:27 +0100

    update length of video

--- R/check.R
+++ R/check.R
@@ -8,7 +8,7 @@ max_num_ass_char <- 800
 linters_to_ignore <- c("trailing_blank_lines_linter", "trailing_whitespace_linter", "line_length_linter", "camel_case_linter")
 
 # how long each exercise takes to complete, in minutes
-timing_lut <- c(VideoExercise = 5,
+timing_lut <- c(VideoExercise = 3.5,
                 NormalExercise = 3,
                 InteractiveExercise = 3,
                 MultipleChoiceExercise = 2,
