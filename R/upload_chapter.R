e083d6fbcd3f9a541e7bdbcdbbbea83912ab69a1
Filip Schouwenaars <filip.schouwenaars@gmail.com>
2016-09-25 17:20:07 +0200

    attempt to fix build

--- R/upload_chapter.R
+++ R/upload_chapter.R
@@ -1,5 +1,5 @@
 #' Create or update a chapter
-#' 
+#'
 #' @param chapter_file path to the Markdown file to be uploaded
 #' @param force boolean, FALSE by default, specifies whether exercises should be removed. If set, will prompt for confirmation.
 #' @param open boolean, TRUE by default, determines whether a browser window should open, showing the course creation web interface
@@ -9,97 +9,94 @@
 #' \dontrun{
 #' # Upload without possibly deleting existing exercises
 #' upload_chapter("chapter1.md")
-#' 
+#'
 #' # Completely sync online chapter with your local markdown file
 #' upload_chapter("chapter1.md", force = TRUE)
 #' }
-#' 
+#'
 #' @export
 upload_chapter = function(chapter_file, force = FALSE, open = TRUE, ask = TRUE) {
 
-  if(!hasArg(chapter_file)) {
-    return(message("Error: You need to specify a chapter Rmd file.")) 
-  }
-  
   old_wd <- getwd()
   setwd(dirname(chapter_file))
   chapter_file <- basename(chapter_file)
-  
+
   course <- load_course_file()
   if (is.null(course$id)) {
     stop("Error: course file does not contain a course id. Please upload your course before uploading chapters.")
   }
-  
-  if(!datacamp_logged_in()) { 
-    datacamp_login() 
+
+  if(!datacamp_logged_in()) {
+    datacamp_login()
   }
-  
+
   if (isTRUE(force) && isTRUE(ask)) {
     sure <- readline("Using 'force' deletes exercises. Are you sure you want to continue? (Y/N) ")
     if (!(sure %in% c("y", "Y", "yes", "Yes"))) { return(message("Aborted.")) }
   }
-  
+
   chapter_index <- get_chapter_index(chapter_file)
   if (length(chapter_index) == 0 && isTRUE(ask)) {
     sure <- readline("Chapter not found in course file. This will create a new chapter, are you sure you want to continue? (Y/N) ")
     if (!(sure %in% c("y", "Y", "yes", "Yes"))) { return(message("Aborted.")) }
   }
-  
+
   message("Parsing R Markdown file ...")
   chapter <- parse_chapter(chapter_file)
-  
+
   message("Converting chapter content to JSON ...")
   output_list <-  list(force = force,
                        skip_validation = TRUE,
                        course = course$id,
                        email = datacamp$get("email"),
                        chapter = chapter)
-  
+
   # Extract chapter id and index from course.yml. If found, add to output_list
   if (length(chapter_index) != 0) { # existing chapter, add info to output_list
     output_list$chapter$id <- as.integer(course$chapters[[chapter_index]])
     output_list$chapter$number <- chapter_index
   }
-  
+
   # Convert entire list to JSON
   chapter_json <- RJSONIO::toJSON(output_list)
-  
+
   message("Uploading chapter to datacamp.com ...")
   upload_chapter_json(chapter_json, chapter_file, open = open) # Upload everything
-  
+
   # reset working directory
   setwd(old_wd)
-  
+
   invisible(NULL)
 }
 
 # Upload the chapter json
 #' @importFrom httr POST content add_headers
+#' @importFrom utils browseURL
 upload_chapter_json = function(chapter_json, chapter_file, open = TRUE) {
   base_url = paste0(datacamp$get("base_url"), "/chapters/create_from_r.json")
   auth_token = datacamp$get("auth_token")
   url = paste0(base_url,"?auth_token=", auth_token)
   x = try(POST(url = url, body = chapter_json, add_headers(c(`Content-Type` = "application/json", `Expect` = ""))))
-  
+
   if (class(x) != "response") {
     stop(no_response)
-  } else { 
-    if (is.list(content(x)) ) { 
-      if ("course" %in% names(content(x)) ) {  
+  } else {
+    if (is.list(content(x)) ) {
+      if ("course" %in% names(content(x)) ) {
         course = content(x)$course
         chapter = content(x)$chapter
         new = content(x)$created
         message(sprintf("Updated course \"%s\" (id: %i)", course$title, course$id))
         if (new == TRUE) {
-          message(sprintf("Created chapter \"%s\" (id: %i)", chapter$title, chapter$id)) 
+          message(sprintf("Created chapter \"%s\" (id: %i)", chapter$title, chapter$id))
         } else {
           message(sprintf("Updated chapter \"%s\" (id: %i)", chapter$title, chapter$id))
         }
         add_chapter_to_course_file(chapter_file, as.integer(chapter$id))
         if (open) {
           browseURL(paste0(datacamp$get("redirect_base_url"), "/", course$id))
-        } 
-      } 
+        }
+      }
       if ("message" %in% names(content(x))) {
         message(content(x)$message)
       }
@@ -110,9 +107,9 @@ upload_chapter_json = function(chapter_json, chapter_file, open = TRUE) {
       }
       if ( "error" %in% names(content(x)) ) {
         message(paste0("Something went wrong:\n", content(x)$error ))
-      } 
+      }
     } else {
       message(paste0("Something went wrong. Please check whether your course was correctly uploaded to datacamp.com."))
-    } 
-  } 
+    }
+  }
 }
--- R/upload_course.R
+++ R/upload_course.R
@@ -1,55 +1,55 @@
 #' Create or update a course
-#' 
-#' Uploads the \code{course.yml} file to datacamp.com. 
+#'
+#' Uploads the \code{course.yml} file to datacamp.com.
 #' Use this function to change the course title, description, etc. and to update the chapters' ordering.
-#' 
+#'
 #' If you're not yet logged in when calling this function, you'll be prompted to log in.
-#' 
+#'
 #' @param course_folder path to folder containing the course yml file.
 #' @param open boolean, TRUE by default, determines whether a browser window should open, showing the course creation web interface
 #' @param force boolean, FALSE by default, that allows to remove chapters from the live course that are not in the course.yml
 #' @param upload_chapters, boolean, FALSE by default, that specifies whether to also upload the chapters
-#' @examples 
+#' @examples
 #' \dontrun{
 #' upload_course()
 #' upload_course(upload_chapters = TRUE) # also upload chapters listed in course.yml
 #' }
 #' @export
-upload_course = function(course_folder = ".", open = TRUE, force = FALSE, upload_chapters = FALSE) { 
-  
+upload_course = function(course_folder = ".", open = TRUE, force = FALSE, upload_chapters = FALSE) {
+
   if (!datacamp_logged_in()) { datacamp_login() }
-  
+
   # Move to folder containing course and chapter files
   old_wd <- getwd()
   setwd(course_folder)
-  
+
   # Build course object from yml
   message("Parsing course file...")
   course <- load_course_file()
-  
+
   if (is.null(course$id)) {
     sure <- readline("No id found in course.yml. This will create a new course, are you sure you want to continue? (Y/N) ")
     if (!(sure %in% c("y", "Y", "yes", "Yes"))) { return(message("Aborted.")) }
   }
-  
+
   if(isTRUE(force)) {
     sure <- readline("Using 'force' will delete chapters online that are not specified in your course.yml. Are you sure you want to continue? (Y/N) ")
     if (!(sure %in% c("y", "Y", "yes", "Yes"))) { return(message("Aborted.")) }
     course$force = TRUE
   }
-  
+
   message("Converting course info to json...")
   course_json <- RJSONIO::toJSON(course)
-  
+
   message("Uploading chapter to datacamp.com ...")
   upload_course_json(course_json, open)
-  
+
   if(upload_chapters) {
     chapter_files_in_yaml <- names(course$chapters)
     if(!is.null(chapter_files_in_yaml)) {
-      lapply(chapter_files_in_yaml, upload_chapter, open = FALSE, force = force, ask = FALSE)  
+      lapply(chapter_files_in_yaml, upload_chapter, open = FALSE, force = force, ask = FALSE)
     }
-    
+
     # Check if chapter files in working directory that are not in course.yml
     chapter_files_in_dir <- dir(pattern = chapter_search_pattern)
     if(length(setdiff(chapter_files_in_dir, chapter_files_in_yaml)) > 0) {
@@ -60,7 +60,7 @@ upload_course = function(course_folder = ".", open = TRUE, force = FALSE, upload
       upload_course(open = FALSE, force = FALSE, upload_chapters = FALSE)
     }
   }
-  
+
   # reset working directory
   setwd(old_wd)
   invisible(NULL)
@@ -69,7 +69,8 @@ upload_course = function(course_folder = ".", open = TRUE, force = FALSE, upload
 
 # Upload the course json
 #' @importFrom httr POST content add_headers
-upload_course_json = function(course_json, open) { 
+#' @importFrom utils browseURL
+upload_course_json = function(course_json, open) {
   base_url = paste0(datacamp$get("base_url"), "/courses/create_from_r.json")
   auth_token = datacamp$get("auth_token")
   url = paste0(base_url,"?auth_token=", auth_token)
@@ -87,7 +88,7 @@ upload_course_json = function(course_json, open) {
           message(sprintf("Updated course \"%s\" (id: %i)", course$title, course$id))
         }
         add_id_to_course_file(course$id) # write id to course.yml file if it's not already there
-        if (open) { 
+        if (open) {
           browseURL(paste0(datacamp$get("redirect_base_url"), "/", course$id))
         }
         if ("message" %in% names(content(x))) {
